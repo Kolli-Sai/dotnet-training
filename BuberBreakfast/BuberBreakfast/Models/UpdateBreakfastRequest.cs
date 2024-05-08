866fb63edd43dadaaba868fe29b929396a9d93ba
Kolli-Sai <saik98187@gmail.com>
2024-05-08 22:58:59 +0530

    CRUD-In Memory with ASP.NET CORE

---
+++ BuberBreakfast/BuberBreakfast/Models/UpdateBreakfastRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace BuberBreakfast;
+
+public class UpdateBreakfastRequest
+{
+    public string Name { get; }
+    public string Description { get; }
+    public DateTime StartDateTime { get; }
+    public DateTime EndDateTime { get; }
+    public List<string> Savory { get; }
+    public List<string> Sweet { get; }
+
+    public UpdateBreakfastRequest(string name, string description, DateTime startDateTime, DateTime endDateTime, List<string> savory, List<string> sweet)
+    {
+
+        Name = name;
+        Description = description;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+
+        Savory = savory;
+        Sweet = sweet;
+    }
+}
